288f186300cebb1407b1b2c29b16b0a5afeb5705
Gabriel Loureiro Fortes Guimarães <gabriel.fortes@intergalaxy.io>
2022-02-22 11:19:56 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+
+
+export function Header(){
+    return(
+       <Container>
+           <header>
+                {/* <img src="" alt="" /> */}
+                <div>
+                    Logo
+                </div>
+                <hr />
+           </header>
+       </Container>
+
+            )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+    header{
+        max-width: 1120px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        padding: 1rem 2rem;
+        color: #fff;
+        background-color: #000;
+        hr{
+            width: 100%;
+        }
+        nav{ 
+
+        }
+    }
+`
