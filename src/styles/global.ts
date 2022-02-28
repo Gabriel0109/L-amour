19307b3fecd41239b05d75845de8c3663d386c51
Gabriel Loureiro Fortes Guimaraes <gabrielglfg@outlook.com>
2022-02-28 15:16:04 -0300

    font types added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -67,10 +67,5 @@ export const GlobalStyle = createGlobalStyle`
     ::selection{
         background-color: rgba(0  ,0  ,0  ,0.1);
     }
-    @font-face {
-        font-family: 'NewYork';
-        src: url('./assets/fonts/NewYork.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-      }
+    
 `
